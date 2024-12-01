fc32c13dc5a881f9cee477515c9c3ca96aa47d6f
Larin Nikita <l_nikita2000@mail.ru>
2024-12-01 13:18:40 +0700

    hero section animation

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,17 +1,37 @@
 import type { ComponentPropsWithRef } from 'react';
 
+import { motion } from 'framer-motion';
 import { twMerge } from 'tailwind-merge';
 
-export const Circle = (props: ComponentPropsWithRef<'div'>) => {
-    const { className, children } = props;
+export const Circle = (
+    props: ComponentPropsWithRef<'div'> & { animate?: boolean },
+) => {
+    const { className, children, animate = false } = props;
 
     return (
         <div
             className={twMerge(
-                'inline-flex size-[240px] items-center justify-center rounded-full bg-zinc-900 outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10',
+                'relative inline-flex size-[240px] items-center justify-center rounded-full bg-zinc-900',
                 className,
             )}
         >
+            <motion.div
+                className={twMerge(
+                    'absolute inset-0 rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10',
+                    animate &&
+                        'border-[6px] border-transparent border-t-fuchsia-500/30',
+                )}
+                animate={
+                    animate && {
+                        rotate: 360,
+                    }
+                }
+                transition={{
+                    duration: 15,
+                    repeat: Infinity,
+                    ease: 'linear',
+                }}
+            />
             {children}
         </div>
     );
